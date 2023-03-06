f15f38d1f7ffb92be7e91b278863c253d6a66ba1
Palki Nanda <you@example.com>
2023-03-06 10:11:50 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll(".input_contact");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
